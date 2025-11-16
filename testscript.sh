025337a9f70666e18184182367667311cd53efc5
Vo Thi Xuan Thao <xuanthao943@gmail.com>
2025-11-16 22:42:58 +0700

    them file sh

---
+++ testscript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script kiểm tra và in thông báo
+echo "Chào bạn! Đây là script test trong nhánh sh."
+
+# In ngày giờ hiện tại
+echo "Thời gian hiện tại:"
+date
+
+# Kiểm tra thư mục hiện tại
+echo "Bạn đang ở thư mục:"
+pwd
